eb43448740af00210004f09065e11e0a2deee381
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-08-04 20:10:43 +0200

    updated changes

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function ContactUs() {
-  return <div>Contac Us page</div>;
+  return (
+    <div className="bg-black text-white p-4 flex flex-col md:flex-row justify-center align-middle">
+      <div className="mr-auto ml-auto text-center">
+        <div className="flex flex-col mb-[2rem]">
+          <h1 className="text-[1.6rem] font-bold">Instagram</h1>
+          <a href="/instagram.com/">
+            <button className="bg-[#41B22F] p-1 rounded-md mt-4 text-sm">
+              Go to Facebook
+            </button>
+          </a>
+        </div>
+      </div>
+
+      <div className="mr-auto ml-auto text-center">
+        <div className="flex flex-col mt-[2rem]">
+          <h1 className="text-[1.6rem] font-bold">Linked In</h1>
+        </div>
+        <a href="/linkedin.com/">
+          <button className="bg-[#41B22F] p-1 rounded-md mt-4 text-sm">
+            Go to LinkedIn
+          </button>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default ContactUs;
--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const Support = () => {
-  return <div>Support Us page</div>;
+  return (
+    <div className="bg-black w-full h-auto text-white p-4">
+      <p>
+        Having successfully completed Phase 1 of our application, a significant
+        milestone in the StreetWyze app development, we are now poised to enter
+        Phase 2. However, to propel our project further, we find ourselves in
+        need of additional capital. The costs of development are pressing,
+        requiring a substantial influx of funds to fully realize the potential
+        of the StreetWyze app as a premier Tourism Safety Application in the
+        market.
+      </p>
+      <p>
+        We warmly invite you to join us on this journey of collaboration. Your
+        support, whether through financial contributions or any other form of
+        assistance, carries immense significance. Every contribution, regardless
+        of its scale, is genuinely valued and deeply appreciated. The collective
+        backing we have received thus far emboldens our pursuit of excellence
+        and fuels our determination.
+      </p>
+      <p>
+        We extend our sincere gratitude to those who have already shown their
+        support and extend an open invitation to become part of the esteemed
+        StreetWyze community. Your partnership is pivotal as we work towards
+        reshaping safety and elevating the tourism experience through our
+        innovative application. We thank you for considering this opportunity to
+        align with our vision and contribute to a shared future.
+      </p>
+    </div>
+  );
 };
 
 export default Support;
