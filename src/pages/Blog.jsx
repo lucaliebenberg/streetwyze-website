e11d36ec77d1abc53f10e462f9d1d5ed56381b33
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-08-04 19:38:24 +0200

    updated new pages

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Blog() {
-  return <div>Blog page</div>;
+  return (
+    <div className="bg-black min-h-[45vh] pt-8 flex justify-center align-middle">
+      <img src="https://picsum.photos/200/300" alt="streetwyze blog" />
+    </div>
+  );
 }
 
 export default Blog;
