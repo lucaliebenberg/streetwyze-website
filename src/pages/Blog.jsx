08ca469def56367eee4e74b227400cac956f0381
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-08-04 19:42:30 +0200

    updated new pages

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Blog() {
   return (
     <div className="bg-black min-h-[45vh] pt-8 flex justify-center align-middle">
-      <img src="https://picsum.photos/200/300" alt="streetwyze blog" />
+      <div className="pb-5">
+        <img src="https://picsum.photos/200/300" alt="streetwyze blog" />
+      </div>
     </div>
   );
 }
