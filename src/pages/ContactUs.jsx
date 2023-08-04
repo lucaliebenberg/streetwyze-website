1f32b7c0bf040396b64d5d1effa56341dd8010c3
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-08-04 20:18:34 +0200

    updated changes

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function ContactUs() {
   return (
-    <div className="bg-black text-white p-4 flex flex-col md:flex-row justify-center align-middle">
+    <div className="bg-black text-white p-4 flex flex-col md:flex-row justify-center align-middle pb-[2rem]">
       <div className="mr-auto ml-auto text-center">
         <div className="flex flex-col mb-[2rem]">
           <h1 className="text-[1.6rem] font-bold">Instagram</h1>
-          <a href="/instagram.com/">
+          <a href="https://www.instagram.com/" target="_blank">
             <button className="bg-[#41B22F] p-1 rounded-md mt-4 text-sm">
               Go to Facebook
             </button>
@@ -18,7 +18,7 @@ function ContactUs() {
         <div className="flex flex-col mt-[2rem]">
           <h1 className="text-[1.6rem] font-bold">Linked In</h1>
         </div>
-        <a href="/linkedin.com/">
+        <a href="https://www.linkedin.com/" target="_blank">
           <button className="bg-[#41B22F] p-1 rounded-md mt-4 text-sm">
             Go to LinkedIn
           </button>
