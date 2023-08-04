6e28767a71bdeca26a746dc1cad2ab0d00afbd1e
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-08-04 19:51:28 +0200

    updated changes

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,6 +5,9 @@ function Blog() {
     <div className="bg-black min-h-[45vh] pt-8 flex justify-center align-middle">
       <div className="pb-5">
         <img src="https://picsum.photos/200/300" alt="streetwyze blog" />
+        <a href="https:x.com/">
+          <button>Got to store</button>
+        </a>
       </div>
     </div>
   );
