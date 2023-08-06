471b334b913eb6e8c0e5dd488ab251ae304918a1
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-08-06 17:54:23 +0200

    project updates

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,25 +1,51 @@
 import React from "react";
 
+// import images
+import InstaLogo from "../assets/instagram-logo.png";
+import LinkedInLogo from "../assets/linkedin-logo.png";
+
 function ContactUs() {
   return (
-    <div className="bg-black text-white p-4 flex flex-col md:flex-row justify-center align-middle pb-[2rem]">
-      <div className="mr-auto ml-auto text-center">
-        <div className="flex flex-col mb-[2rem]">
-          <h1 className="text-[1.6rem] font-bold">Instagram</h1>
+    <div className="bg-black text-white p-4 flex flex-col md:flex-row justify-center align-middle pt-[4rem] pb-[4rem]">
+      {/* <div className="mr-auto ml-auto text-center">
+        <div className="flex flex-col mt-[2rem] mb-[2rem]">
+          <h1 className="text-[1.6rem] font-bold mb-4">Instagram</h1>
+          <img src={InstaLogo} alt="Instagram logo" height="250" width="250" />
           <a href="https://www.instagram.com/" target="_blank">
-            <button className="bg-[#41B22F] p-1 rounded-md mt-4 text-sm">
+            <button className="bg-[#41B22F] p-1 rounded-md mt-4 text-sm w-full h-[4vh] md:h-[5vh]">
               Go to Instagram
             </button>
           </a>
         </div>
+      </div> */}
+      <div className="mr-auto ml-auto text-center">
+        <div className="flex flex-col mt-[2rem] mb-[2rem]">
+          <h1 className="text-[1.6rem] md:text-[2rem] font-bold mb-3">
+            Instagram
+          </h1>
+          <img src={InstaLogo} alt="Instagram logo" height="250" width="250" />
+        </div>
+        <a href="https://www.linkedin.com/" target="_blank">
+          <button className="bg-[#41B22F] p-1 rounded-md mt-4 text-lg w-full h-[4vh] md:h-[5vh]">
+            Go to Instagram
+          </button>
+        </a>
       </div>
 
       <div className="mr-auto ml-auto text-center">
-        <div className="flex flex-col mt-[2rem]">
-          <h1 className="text-[1.6rem] font-bold">Linked In</h1>
+        <div className="flex flex-col mt-[2rem] mb-[2rem]">
+          <h1 className="text-[1.6rem] md:text-[2rem] font-bold mb-3">
+            Linked In
+          </h1>
+          <img
+            src={LinkedInLogo}
+            alt="LinkedIn logo"
+            height="250"
+            width="250"
+          />
         </div>
         <a href="https://www.linkedin.com/" target="_blank">
-          <button className="bg-[#41B22F] p-1 rounded-md mt-4 text-sm">
+          <button className="bg-[#41B22F] p-1 rounded-md mt-4 text-lg w-full h-[4vh] md:h-[5vh]">
             Go to LinkedIn
           </button>
         </a>
