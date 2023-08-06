993a7f57f5e7b4736785b7614c0242bb3d3fb9db
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-08-06 17:40:51 +0200

    project updates

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -2,32 +2,37 @@ import React from "react";
 
 const Support = () => {
   return (
-    <div className="bg-black w-full h-auto text-white p-4">
-      <p>
-        Having successfully completed Phase 1 of our application, a significant
-        milestone in the StreetWyze app development, we are now poised to enter
-        Phase 2. However, to propel our project further, we find ourselves in
-        need of additional capital. The costs of development are pressing,
-        requiring a substantial influx of funds to fully realize the potential
-        of the StreetWyze app as a premier Tourism Safety Application in the
-        market.
-      </p>
-      <p>
-        We warmly invite you to join us on this journey of collaboration. Your
-        support, whether through financial contributions or any other form of
-        assistance, carries immense significance. Every contribution, regardless
-        of its scale, is genuinely valued and deeply appreciated. The collective
-        backing we have received thus far emboldens our pursuit of excellence
-        and fuels our determination.
-      </p>
-      <p>
-        We extend our sincere gratitude to those who have already shown their
-        support and extend an open invitation to become part of the esteemed
-        StreetWyze community. Your partnership is pivotal as we work towards
-        reshaping safety and elevating the tourism experience through our
-        innovative application. We thank you for considering this opportunity to
-        align with our vision and contribute to a shared future.
-      </p>
+    <div className="bg-black w-full h-auto text-white p-4 pt-[2rem]">
+      <div className=" ml-auto mr-auto">
+        <h1 className="text-lg md:text-[2rem] font-bold p-8 md:p-16 mb:pb-0">
+          Support Us
+        </h1>
+        <p className="w-full p-4 text-[1.2rem] md:p-16 md:pb-16 md:pt-0">
+          Having successfully completed Phase 1 of our application, a
+          significant milestone in the StreetWyze app development, we are now
+          poised to enter Phase 2. However, to propel our project further, we
+          find ourselves in need of additional capital. The costs of development
+          are pressing, requiring a substantial influx of funds to fully realize
+          the potential of the StreetWyze app as a premier Tourism Safety
+          Application in the market.
+        </p>
+        <p className="w-full p-4 text-[1.2rem] md:p-16 md:pt-1  md:pb-16">
+          We warmly invite you to join us on this journey of collaboration. Your
+          support, whether through financial contributions or any other form of
+          assistance, carries immense significance. Every contribution,
+          regardless of its scale, is genuinely valued and deeply appreciated.
+          The collective backing we have received thus far emboldens our pursuit
+          of excellence and fuels our determination.
+        </p>
+        <p className="w-full p-4 text-[1.2rem] md:p-16 md:pt-1">
+          We extend our sincere gratitude to those who have already shown their
+          support and extend an open invitation to become part of the esteemed
+          StreetWyze community. Your partnership is pivotal as we work towards
+          reshaping safety and elevating the tourism experience through our
+          innovative application. We thank you for considering this opportunity
+          to align with our vision and contribute to a shared future.
+        </p>
+      </div>
     </div>
   );
 };
