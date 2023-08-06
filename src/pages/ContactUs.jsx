1fd278bb780d2c2b0ec178c20b13bbc16fa745a7
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-08-06 18:21:58 +0200

    project updates

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -7,17 +7,6 @@ import LinkedInLogo from "../assets/linkedin-logo.png";
 function ContactUs() {
   return (
     <div className="bg-black text-white p-4 flex flex-col md:flex-row justify-center align-middle pt-[4rem] pb-[4rem]">
-      {/* <div className="mr-auto ml-auto text-center">
-        <div className="flex flex-col mt-[2rem] mb-[2rem]">
-          <h1 className="text-[1.6rem] font-bold mb-4">Instagram</h1>
-          <img src={InstaLogo} alt="Instagram logo" height="250" width="250" />
-          <a href="https://www.instagram.com/" target="_blank">
-            <button className="bg-[#41B22F] p-1 rounded-md mt-4 text-sm w-full h-[4vh] md:h-[5vh]">
-              Go to Instagram
-            </button>
-          </a>
-        </div>
-      </div> */}
       <div className="mr-auto ml-auto text-center">
         <div className="flex flex-col mt-[2rem] mb-[2rem]">
           <h1 className="text-[1.6rem] md:text-[2rem] font-bold mb-3">
