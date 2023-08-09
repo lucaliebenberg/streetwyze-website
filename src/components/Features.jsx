5c905674f45a8d9194bcac6dcdc8264b8ae809ac
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-08-09 21:52:29 +0200

    updated features copy & icon

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 // import icons
 import { GrRestaurant } from "react-icons/gr";
+import { BiSolidBot } from "react-icons/bi";
 
 // import icons
 import Feature1 from "../assets/Icon.svg";
@@ -29,18 +30,36 @@ const Features = () => {
       </h1>
 
       <div className="flex flex-col mt-0 pt-0 p-[5rem] h-full md:flex-row md:justify-between lg:mt-auto lg:mb-auto">
-        <div className="flex flex-col mr-2 lg:mt-14 xl:mr-0">
-          <img
-            src={Feature1}
-            alt="Feature 1 icon"
-            className="h-[40px] w-[40px] ml-auto mr-auto mb-10"
-          />
-          <h2 className="text-[20px] md:text-[22px] text-[#137703] font-bold justify-center text-center mb-4">
-            Colour-coded
-          </h2>
-          <p className="text-[13px] md:text-[16px] md:text-center text-[#ABABAB] justify-center text-center max-w-[277px] mr-auto ml-auto">
-            We provide a analytical colour-coded interface of Cape Town
-          </p>
+        <div>
+          <div className="flex flex-col mt-14">
+            <img
+              src={Feature5}
+              alt="Feature 3 icon"
+              className="h-[40px] w-[40px] ml-auto mr-auto mb-10"
+            />
+            <h2 className="text-[20px] md:text-[22px] text-[#137703] font-bold justify-center text-center mb-4">
+              Restaurant recommendations
+            </h2>
+            <p className="text-[13px] md:text-[16px] text-[#ABABAB] justify-center text-center max-w-[280px] mr-auto ml-auto">
+              Discover culinary wonders with curated restaurant recommendations
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="flex flex-col mt-14">
+            <img
+              src={Feature5}
+              alt="Feature 3 icon"
+              className="h-[40px] w-[40px] ml-auto mr-auto mb-10"
+            />
+            <h2 className="text-[20px] md:text-[22px] text-[#137703] font-bold justify-center text-center mb-4">
+              Activities recommendations
+            </h2>
+            <p className="text-[13px] md:text-[16px] text-[#ABABAB] justify-center text-center max-w-[260px] mr-auto ml-auto">
+              Explore activities with StreetWyze's reliable information
+            </p>
+          </div>
         </div>
 
         <div>
@@ -58,35 +77,19 @@ const Features = () => {
             </p>
           </div>
         </div>
-        <div>
-          <div className="flex flex-col mt-14">
-            <img
-              src={Feature5}
-              alt="Feature 3 icon"
-              className="h-[40px] w-[40px] ml-auto mr-auto mb-10"
-            />
-            <h2 className="text-[20px] md:text-[22px] text-[#137703] font-bold justify-center text-center mb-4">
-              Restaurant recommendations
-            </h2>
-            <p className="text-[13px] md:text-[16px] text-[#ABABAB] justify-center text-center max-w-[280px] mr-auto ml-auto">
-              Discover culinary wonders with curated restaurant recommendations
-            </p>
-          </div>
-        </div>
-        <div>
-          <div className="flex flex-col mt-14">
-            <img
-              src={Feature5}
-              alt="Feature 3 icon"
-              className="h-[40px] w-[40px] ml-auto mr-auto mb-10"
-            />
-            <h2 className="text-[20px] md:text-[22px] text-[#137703] font-bold justify-center text-center mb-4">
-              Activities recommendations
-            </h2>
-            <p className="text-[13px] md:text-[16px] text-[#ABABAB] justify-center text-center max-w-[260px] mr-auto ml-auto">
-              Explore activities with StreetWyze's reliable information
-            </p>
-          </div>
+
+        <div className="flex flex-col mr-2 lg:mt-14 xl:mr-0">
+          <BiSolidBot
+            alt="Feature 1 icon"
+            className="h-[40px] w-[40px] ml-auto mr-auto mb-10 text-[#137703]"
+            color="[#137703]"
+          />
+          <h2 className="text-[20px] md:text-[22px] text-[#137703] font-bold justify-center text-center mb-4">
+            Chatbot safety reports
+          </h2>
+          <p className="text-[13px] md:text-[16px] md:text-center text-[#ABABAB] justify-center text-center max-w-[277px] mr-auto ml-auto">
+            Make use of our AI powered chatbot to receive real time support
+          </p>
         </div>
       </div>
     </div>
